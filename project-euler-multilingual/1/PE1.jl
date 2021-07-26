08502a59d3a6c70fd4f1f4d4bbd2a52115fa927f
yelircaasi <isaac.r.riley@gmail.com>
2021-07-26 12:07:45 +0200

    added a few js files

--- project-euler-multilingual/1/PE1.jl
+++ project-euler-multilingual/1/PE1.jl
@@ -1,3 +1,42 @@
-'''
-Julia
-'''
+"""
+Julia solution to Project Euler problem 1
+
+Inspired by the approach at https://github.com/johnmyleswhite/projecteuler.jl/blob/master/001.jl
+"""
+
+
+# Enumerate the multiples of n up to some strict upper bound.
+function bounded_multiples(bound, n)
+  if rem(bound, n) == 0
+    0:n:(n * (div(bound, n) - 1))
+  else
+    0:n:(n * div(bound, n))
+  end
+end
+
+"""
+# Confirm that approach works for natural numbers below 10
+upper_bound = 10
+multiples_of_3 = bounded_multiples(upper_bound, 3)
+multiples_of_5 = bounded_multiples(upper_bound, 5)
+multiples_of_15 = bounded_multiples(upper_bound, 15)
+sum(multiples_of_3) + sum(multiples_of_5) - sum(multiples_of_15)
+"""
+
+# Use to solve problem for numbers below 1000
+upper_bound = 1000
+multiples_of_3 = bounded_multiples(upper_bound, 3)
+multiples_of_5 = bounded_multiples(upper_bound, 5)
+multiples_of_15 = bounded_multiples(upper_bound, 15)
+total_sum = sum(multiples_of_3) + sum(multiples_of_5) - sum(multiples_of_15)
+
+# Slow code to serve as an error checking mechanism.
+total_sum_check = 0
+for n in 1:(upper_bound-1)
+  if rem(n, 3) == 0 || rem(n, 5) == 0
+    global total_sum_check += n
+  end
+end
+
+println("Total sum: $total_sum")
+println("Check: $total_sum_check")
--- project-euler-multilingual/1/PE1.py
+++ project-euler-multilingual/1/PE1.py
@@ -8,18 +8,17 @@ https://projecteuler.net/problem=1
 
 total = 0
 for n in range(1000):
-  if n%3==0 or n%5==0:
-    total += n
-   
-#alternative solution 1: 
-total2 = sum(n for n in range(1000) if (n%3)*(n%5)==0)  
+    if n % 3 == 0 or n % 5 == 0:
+        total += n
 
-#alternative solution 2:
-modsum = lambda mod:mod*sum(range(1000//mod+1))
+# alternative solution 1:
+total2 = sum(n for n in range(1000) if (n % 3)*(n % 5) == 0)
+
+# alternative solution 2:
+def modsum(mod): return mod*sum(range(1000//mod + bool(1000%mod)))
+
 total3 = modsum(3)+modsum(5)-modsum(15)
 
 print("First solution:\t\t", total)
 print("Alternative solution 1:\t", total2)
 print("Alternative solution 2:\t", total3)
-
-
